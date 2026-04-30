c222de4e46b4bc2a5aa1399621795c829fba00e6
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2026-04-30 12:51:02 +0100

    almost there

--- foreach-loop.cpp
+++ foreach-loop.cpp
@@ -4,5 +4,17 @@ int main(){
     // foreach loop = loop that eases the traversal over an 
     //                iterable data set
 
+    std::string people[] = {"Ore", "Ayo", "Jeremiah", "Segun"};
+
+    for(std::string i : people ){
+        std::cout << i << ",";
+    }
+
+    int grades[] = {91,77,98,90,99,100};
+
+    for(int grade: grades){
+        std::cout << grade << '\n';
+    }
+
     return 0;
 }
