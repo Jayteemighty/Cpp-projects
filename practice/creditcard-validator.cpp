b361d00a2e761446c94c9bbf9436b2bf433ac430
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2026-05-03 22:43:21 +0100

    Im unstopable

---
+++ practice/creditcard-validator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    // luhn algorithm
+    // 1. Starting from the right, double every second digit
+    // If doubling gives a number > 9, subtract 9
+    // Add all digits together
+    // If total % 10 == 0 → ✅ valid card
+    return 0;
+}
