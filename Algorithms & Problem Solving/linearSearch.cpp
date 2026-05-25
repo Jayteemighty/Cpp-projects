4720ef6db2ff8401e66e64f44e34740d114e777a
unknown <tolujosh1@gmail.com>
2026-05-25 22:16:59 +0100

    Linear Search ++

--- Algorithms & Problem Solving/linearSearch.cpp
+++ Algorithms & Problem Solving/linearSearch.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    std::cout << "New!";
+    // This is a simple linear search algorithm 
+    
+    int arr[] = {5,8,2,7,3};
+
+    int userInput;
+    bool found = false;
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Enter value to look for in the array: ";
+    cin >> userInput;
+
+    for(int i =0; i < size; i++){
+        if (arr[i] == userInput){
+            cout << "Value " << userInput << " found at index: " << i << '\n';
+            found = true;
+            break;
+        }
+        
+    }
+    if(!found){
+        cout << "Value " << userInput << " not found in the array.\n";
+    }
     return 0;
 }
