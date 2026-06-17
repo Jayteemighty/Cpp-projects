5356609ccdf78536d33844f033854ead03bd9639
unknown <tolujosh1@gmail.com>
2026-06-17 11:35:49 +0100

    traffic light pratice

---
+++ practice/traffic_light_controller.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+// Traffic Light Controller
+// This program simulates a traffic light controller for an intersection.
+
+enum TrafficLight{
+        RED,
+        GREEN,
+        YELLOW,
+    };
+
+void displayTrafficLight(constTrafficLight &light){
+    switch (light){
+        case RED:
+            cout << "Current Light: RED\n";
+            cout << "STOP!\n";
+            break;
+        case GREEN:
+            cout << "Current Light: GREEN\n";
+            cout << "GO!\n";
+            break;
+        case YELLOW:
+            cout << "Current Light: YELLOW\n";
+            cout << "SLOW DOWN!\n";
+            break;
+    }
+}
+
+void changeTrafficLight(constTrafficLight &light){
+    switch (light){
+        case RED:
+            light = GREEN;
+            break;
+        case GREEN:
+            light = YELLOW;
+            break;
+        case YELLOW:
+            light = RED;
+            break;
+    }
+}
+
+void showMenu(){
+    cout << "**************Traffic Light Controller Menu:***************\n";
+    cout << "1. Display current traffic light\n";
+    cout << "2. Change traffic light\n";
+    cout << "3. Exit\n";
+    cout << "**********************************************************\n";
+    cout << "Enter your choice: ";
+    
+}
+int main(){
+    TrafficLight light = RED;
+    int choice;
+
+    do{
+        showMenu();
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+                displayTrafficLight(light);
+                break;
+            case 2:
+                changeTrafficLight(light);
+                displayTrafficLight(light);
+                break;
+            case 3:
+                cout << "Exiting the program. Goodbye!\n";
+                break;
+            default:
+                cout << "Invalid choice! Please try again.\n";
+        }
+    }while(choice != 3);
+
+    return 0;
+}
