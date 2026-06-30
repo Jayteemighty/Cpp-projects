93d6ac5354a4105edbd005c631ded306be0869b9
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2026-06-30 15:41:46 +0000

    first

---
+++ practice/motor_control_system.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void showMenu(){
+    cout << "=============================";
+    cout << "MOTOR CONTROL SYSTEM";
+    cout << "============================="
+    cout << 
+    1. Start Motor
+    2. Stop Motor
+    3. Increase Speed
+    4. Decrease Speed
+    5. Change Direction
+    6. Emergency Stop
+    7. Show Status
+    8. Exit
+}
+
+int main(){
+
+    return 0;
+}
