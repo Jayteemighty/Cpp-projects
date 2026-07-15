051bfdc1c654928ab14d078cf256125a35f0ad50
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2026-07-15 12:46:49 +0000

    enjoy

--- practice/motor_control_system.cpp
+++ practice/motor_control_system.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
+#include <string>
+#include <limits>
 using namespace std;
 
-void showMenu(){
+enum class MotorState{
+    OFF,
+    ON
+};
+
+enum class MotorDirection{
+    FORWARD,
+    REVERSE
+};
+
+class Motor{
+    private:
+    MotorState state;
+    MotorDirection direction;
+    int speed;
+
+    static constexpr int MIN_SPEED = 0;
+    static constexpr int MAX_SPEED = 100;
+    static constexpr int SPEED_STEP = 10;
+
+    public:
+    // Initial state
+    Motor(): state(MotorState::OFF),
+             direction(MotorDirection::FORWARD),
+             speed(0){
+                std::cout << "\nMotor initialized: OFF, Speed=0%, Direction=FORWARD\n";
+             }
+    
+    void startMotor(){
+
+    }
+
+    void stopMotor(){
+
+    }
+
+    void increaseSpeed(){
+
+    }
+
+    void decreaseSpeed(){
+
+    }
+
+    void changeDirection(){
+
+    }
+
+    void emergencyStop(){
+
+    }
+
+    void showStatus(){
+
+    }
+
+
+};
+
+void displayMenu(){
     cout << "=============================";
     cout << "MOTOR CONTROL SYSTEM";
     cout << "=============================";
